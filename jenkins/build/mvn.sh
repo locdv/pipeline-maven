a8f0f9b8d72afc61ddbae81045ff49f6ce56f61f
locdv <locdv@localhost.localdomain>
2022-06-08 11:09:36 -0400

    update mvn

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,6 +3,6 @@
 echo "*********************"
 echo "****Build jar********"
 echo "*******************"
-WORKSPACE=/home/locdv/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/locdv/jenkins-data/jenkins_home/workspace/pipeline-maven-docker
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8 "$@"
